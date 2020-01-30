5919cf990bad467b8a52ec57f5be5d05945eb061
djChris4Training <chris.de.jonge@be.ibm.com>
2020-01-30 10:38:41 +0000

    missing

--- tests/product-service.test.js
+++ tests/product-service.test.js
@@ -1,13 +1,12 @@
-// tests/product-service.test.js
 /**
-* @jest-environment node
-*/
+ * @jest-environment node
+ */
 var productService = require('../src/services/product-service')
 var nock = require('nock')
 
 describe('Product service', function () {
   it('Should call remote service', async function () {
-    nock('https://product.service:8899/products')
+    nock('https://mycluster.icp:8899/products')
       .get('/13')
       .delayBody(10)
       .reply(200, {
@@ -18,21 +17,21 @@ describe('Product service', function () {
     let weight = await productService.getProductWeight('13')
     expect(weight).toBe(15.5)
   })
+
   it('Should handle unexpected response structure', async function () {
-    nock('https://product.service:8899/products')
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get('/19')
-        .reply(200, {
-          res: 15.5
-        })
-  
+    nock('https://mycluster.icp:8899/products')
+      .get('/19')
+      .reply(200, {
+        res: 15.5
+      })
+
     await productService
-        .getProductWeight('19')
-        .then(() => {
-          throw(new Error('Should not resolve in case of malformed data'))
-        })
-        .catch(err => {
-          expect(err.message).toBe('Invalid response object')
-        })
-    })
+      .getProductWeight('19')
+      .then(() => {
+        throw(new Error('Should not resolve in case of malformed data'))
+      })
+      .catch(err => {
+        expect(err.message).toBe('Invalid response object')
+      })
+  })
 })
